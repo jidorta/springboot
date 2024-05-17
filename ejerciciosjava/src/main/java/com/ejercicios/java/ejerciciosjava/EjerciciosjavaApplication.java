1b71bbca3b245e34808d8639b989d7f2988e8c72
ibandorta <ibandorta@gmail.com>
2024-05-17 09:38:22 +0200

    Inicio ejercicios

---
+++ ejerciciosjava/src/main/java/com/ejercicios/java/ejerciciosjava/EjerciciosjavaApplication.java
@@ -0,0 +1,13 @@
+package com.ejercicios.java.ejerciciosjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjerciciosjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjerciciosjavaApplication.class, args);
+	}
+
+}
---
+++ ejerciciosjava/src/test/java/com/ejercicios/java/ejerciciosjava/EjerciciosjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejercicios.java.ejerciciosjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjerciciosjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
